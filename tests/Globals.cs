b81973b0fe83aa62d0da30ffd3ebb358e60c93d1
Ken Brittain <ken.brittain@hotmail.com>
2022-08-31 20:11:40 -0400

    Add globals so I don't have to import in most files

---
+++ tests/Globals.cs
@@ -0,0 +1,6 @@
+global using global::System;                     // for all files (if needed or not)
+global using global::System.Collections.Generic; // obvious reasons
+global using global::System.Linq;                // makes playing with collections better
+global using global::System.Threading.Tasks;     // mostly for web projects
+global using global::Xunit;                      // unit tests
+global using global::Moq;                        // mocking lib
